041cebb8a79c3bd10fc1109ba3952e83488e0eb7
cactixxx <eduplooy@gmail.com>
2021-05-17 21:05:38 +0200

    Update renew_letsencrypt.sh

--- renew_letsencrypt.sh
+++ renew_letsencrypt.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+# Renew the certificate
+/usr/bin/certbot renew --force-renewal -q
+/usr/bin/cat /etc/letsencrypt/live/zeedesigns.co.za/fullchain.pem /etc/letsencrypt/live/zeedesigns.co.za/privkey.pem > /etc/ssl/zeedesigns.co.za/zeedesigns.co.za.pem
+
+# Reload  HAProxy
+/usr/sbin/service haproxy reload
