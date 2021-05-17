92e372b86e45b9b2ddde313656cb20284e948d04
cactixxx <eduplooy@gmail.com>
2021-05-17 21:33:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 echo " "
-echo "Installation script for zeedesigns"
+echo "Installation script for ZeeDesigns"
+apt update
+apt upgrade
 echo " "
-echo "Please update the server before installation..."
-echo " "
-echo "Press any key to continue or CRRL-C to end the installation"
+echo "If kernel updates were installed please press CTRL-C and reboot"
 read -n 1 -s
-echo pop
+
+sed -i 's/ubuntu/zeedesigns/' /etc/hosts
+sudo apt install openjdk-11-jdk
+wget https://github.com/thingsboard/thingsboard/releases/download/v3.2.2/thingsboard-3.2.2.deb
+sudo dpkg -i thingsboard-3.2.2.deb
+sudo apt install -y wget vim mlocate
+
+#Postgres
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add
+RELEASE=$(lsb_release -cs)
+echo "deb http://apt.postgresql.org/pub/repos/apt/ ${RELEASE}"-pgdg main | sudo tee  /etc/apt/sources.list.d/pgdg.list
+sudo apt update
+sudo apt -y install postgresql-12
+sudo service postgresql start
