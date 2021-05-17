8f209f3aaa9df2d02dbe951e18e5cfc9fde73325
cactixxx <eduplooy@gmail.com>
2021-05-17 21:16:42 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo " "
+echo "Installation script for zeedesigns"
+echo " "
+echo "Please update the server before installation..."
+echo " "
+echo "Press any ket to continue or CRRL-C to end the installation"
+read -n 1 -s'
+echo pop
