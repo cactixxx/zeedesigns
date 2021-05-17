b920236c83893ba2cbd12699a19a02386b69c78e
cactixxx <eduplooy@gmail.com>
2021-05-17 21:23:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ echo "Installation script for zeedesigns"
 echo " "
 echo "Please update the server before installation..."
 echo " "
-echo "Press any ket to continue or CRRL-C to end the installation"
+echo "Press any key to continue or CRRL-C to end the installation"
 read -n 1 -s
 echo pop
